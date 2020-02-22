ad5a35761f81326663657df2a4bd5b6721e5dfa7
Maura Slavin <MauraMSlavin@gmail.com>
2020-02-22 16:40:42 -0500

    Skeleton of components set up.

---
+++ src/components/Saved.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Book from "./Book.js"
+
+
+function Saved() {
+  return (
+    <div>
+        Saved
+        <Book />
+    </div>
+  );
+}
+
+export default Saved;
